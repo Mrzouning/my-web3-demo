37747cb7bdfa55e3a26f24c6f31218c0d2577cc3
zou ning <mrzouning@163.com>
2022-09-04 20:42:13 +0800

    feat: update my-web3-project

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,63 @@
-import React from "react";
-import logo from "./logo.svg";
+import { useEffect } from "react";
+import { useWeb3React } from "@web3-react/core";
+import { injected } from "./connectors";
 
 function App() {
+  const { active, account, library, connector, activate, deactivate } =
+    useWeb3React();
+
+  async function connect() {
+    console.log(1111);
+    try {
+      activate(injected);
+      localStorage.setItem("isWalletConnected", "true");
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  async function disconnect() {
+    try {
+      deactivate();
+      localStorage.setItem("isWalletConnected", "false");
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    const connectWalletOnload = async () => {
+      if (localStorage.getItem("isWalletConnected")) {
+        try {
+          await activate(injected);
+        } catch (error) {
+          console.log(error);
+        }
+      }
+    };
+    connectWalletOnload();
+  }, []);
+  console.log(account);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
+    <div className="flex flex-col items-center justify-center">
+      <button
+        className="py-2 mt-20 mb-4 text-lg font-bold text-white rounded-lg w-56 bg-blue-600 hover:bg-blue-800"
+        onClick={connect}>
+        Connect Wallet
+      </button>
+      {active ? (
+        <span>
+          Connected with<b>{account}</b>
+        </span>
+      ) : (
+        <span>Not connected</span>
+      )}
+      <button
+        className="py-2 mt-20 mb-4 text-lg font-bold text-white rounded-lg w-56 bg-gray-600 hover:bg-blue-800"
+        onClick={disconnect}>
+        Disconnect
+      </button>
     </div>
   );
 }
